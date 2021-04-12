00289835c7eae447fe16ab17bad7afb5ca76a9f8
Matthieu Blais <matthieu.blais1@gmail.com>
2021-04-12 23:28:05 +0800

    testing module to test

--- 04-testing/client.py
+++ 04-testing/client.py
@@ -2,29 +2,54 @@ import requests
 from jsonschema import validate
 import boto3
 import yaml
+import os
+from datetime import datetime
 
 class APIClient():
 
-    def __init__(self, host, spec_bucket, spec_key):
+    def __init__(self, host):
         self.host = host
         self.s3 = boto3.client('s3', region_name=os.environ["AWS_REGION"])
+        self.report = {
+            "details": {
+                "start_date": datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
+            },
+            "cases": []
+        }
+        
+    def run_tests(self, spec_bucket, spec_key):
         self.endpoints = self._download_endpoints(spec_bucket, spec_key)
-
-    def run_tests(self):
+        self.report["details"]["bucket"] = spec_bucket
+        self.report["details"]["key"] = spec_key
+        success = True
         for endpoint in self.endpoints:
-            req = self.call(endpoint['method'], endpoint['method'])
+            case = self.test_endpoint(endpoint)
+            success = success and case["success"]
+            self.report["cases"].append(case)
+        self.report["details"]["end_date"] = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
 
     def test_endpoint(self, endpoint):
-        for testcase in endpoint.get("tests", []):
-            req = self.call(
-                testcase.get('method', endpoint.get("method")), 
-                endpoint.get('endpoint'),
-                testcase.get('headers', endpoint.get("headers")),
-                testcase.get('data')
-            )
-            req_validity = ResponseValidation.validate(req, testcase.get('response'))
+        results = []
+        for idx, testcase in enumerate(endpoint.get("tests", [])):
+            request = self._format_request(endpoint, testcase)
+            assertions = self._format_assertions(endpoint, testcase.get("response", {}))
+            req = self.call(**request)
+            validity, report = ResponseValidation.validate(req, assertions)
+            results.append({
+                "endpoint": endpoint.get('endpoint'),
+                'method': endpoint.get("method"),
+                "test_id": idx,
+                "success": validity,
+                "details": report
+            })
+        return results
 
-            
+    def upload_report(self, bucket, key, report):
+        self.s3.put_object(
+            Bucket=bucket,
+            Key=key,
+            Body=json.dumps(self.report)
+        )
 
     def call(self, method, endpoint, headers={}, data=None):
         return requests.request(method, f"{self.host}{endpoint}", headers=headers, data=data)
@@ -34,36 +59,85 @@ class APIClient():
         obj = self.s3.get_object(Bucket=bucket, Key=key)
         return yaml.safe_load(obj["Body"].read())
 
+    def _format_request(self, endpoint, testcase):
+        return {
+            'method': endpoint.get("method"), 
+            'endpoint': endpoint.get('endpoint'),
+            'headers': testcase.get('headers', endpoint.get("headers", {})),
+            'data': testcase.get('data')
+        }
+
+    def _format_assertions(self, endpoint, assertion):
+        return {
+            'code': assertion.get("code", 200), 
+            'headers': assertion.get('headers', {}),
+            'schema': testcase.get('schema', endpoint.get("schema", None)),
+            'body': testcase.get('body', None)
+        }
+
 
 
 class ResponseValidation:
 
     @classmethod
     def validate(cls, request, assertions):
-        if "code" in assertions:
-            cls._response_code(request.status_code, assertions["code"])
-        cls._response_code()
-        if "headers" in assertions:
-            headers = assertions["headers"]
-            cls._headers(request.headers, headers)
-        if "schema" in assertions:
-            cls._schemas(request.json(), assertions['schema'])
-        if "body" in assertions:
-            cls._body(request.headers, request.text, assertions['body'])
+        report = {
+            "code": None,
+            "headers": None,
+            "schema": None,
+            "body": None
+        }
+        success = True
+        if assertions["code"]:
+            code_validation = cls._response_code(request.status_code, assertions["code"])
+            success = success and code_validation
+            report["code"] = {
+                "raw": request.status_code,
+                "success": code_validation
+            }
+        if assertions["headers"]:
+            headers_validation = cls._headers(request.headers, assertions["headers"])
+            success = success and headers_validation
+            report["headers"] = {
+                "raw": request.headers,
+                "success": headers_validation
+            }
+        if assertions["schema"]:
+            schema_validation, err = cls._schemas(request.json(), assertions['schema'])
+            success = success and schema_validation
+            report["schema"] = {
+                "raw": request.json(),
+                "success": schema_validation,
+            }
+            if err:
+                report["schema"]["details"] = err
+        if assertions["body"]:
+            body_validation = cls._body(request.json(), assertions['body'])
+            success = success and body_validation
+            report["schema"] = {
+                "raw": request.json(),
+                "success": body_validation
+            }
+        return success, report
 
 
     @classmethod
     def _headers(cls, headers, assertions):
         for key, value in assertions.items():
+            if key not in 
             assert key in headers and headers[key] == value
 
     @classmethod
     def _response_code(cls, code, assertion):
-        assert code == assertion
+        return code == assertion:
 
     @classmethod
     def _schemas(cls, body, assertion):
-        validate(instance=body, schema=assertion)
+        try:
+            validate(instance=body, schema=assertion)
+            return True, None
+        except Exception as e:
+            return False, str(e)
 
     @classmethod
     def _body(cls, headers, body, assertion):
--- 04-testing/main.py
+++ 04-testing/main.py
@@ -1,15 +1,18 @@
+from client import APIClient
 
 
-
-
-
-
+def api_testing(host, testcases_bucket, testcases_key, result_bucket, result_key):
+    api = APIClient(host)
+    api.run_tests(bucket, key)
+    api.upload_report(result_bucket, result_key)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", "-H")
-    parser.add_argument("--protocol-bucket", default=None)
-    parser.add_argument("--protocol-key", default=None)
+    parser.add_argument("--testcases-bucket", default=None)
+    parser.add_argument("--testcases-key", default=None)
+    parser.add_argument("--result-bucket", default=None)
+    parser.add_argument("--result-key", default=None)
     args = parser.parse_args()
 
     print(f"Configuration: host={args.host}, protocol-bucket={args.protocol_bucket}, protocol-key={args.protocol_key}")
